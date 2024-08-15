0af5d5b7db95df68fb0d9827f5bdfe1636e2cb7a
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-15 00:05:37 -0300

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -1,1 +1,10 @@
+psql --username=freecodecamp --dbname=postgres
 
+CREATE DATABASE worldcup;
+
+\c worldcup;
+
+CREATE TABLE teams (team_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL);
+
+CREATE TABLE games (game_id SERIAL PRIMARY KEY NOT NULL, year INT NOT NULL, round VARCHAR(50) NOT NULL, 
+winner_goals INT NOT NULL, opponent_goals INT NOT NULL, winner_id INT NOT NULL, opponent_id INT NOT NULL); 
