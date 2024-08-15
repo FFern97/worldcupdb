b3bd9f415a5c406de592a2b3b96b7d53bdf3063c
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-15 00:09:55 -0300

    Update worldcup.sql

--- worldcup.sql
+++ worldcup.sql
@@ -4,7 +4,7 @@ CREATE DATABASE worldcup;
 
 \c worldcup;
 
-CREATE TABLE teams (team_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL);
+CREATE TABLE teams (team_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL UNIQUE);
 
 CREATE TABLE games (game_id SERIAL PRIMARY KEY NOT NULL, year INT NOT NULL, round VARCHAR(50) NOT NULL, 
 winner_goals INT NOT NULL, opponent_goals INT NOT NULL, winner_id INT NOT NULL, opponent_id INT NOT NULL); 
