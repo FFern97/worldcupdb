b9e54523413f2373a0e8b5fda3b63068737cb8ca
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-20 22:34:24 -0300

    Create Insert_data.sh

---
+++ Insert_data.sh
@@ -0,0 +1,23 @@
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+
+    
+    WINNER_ID=$($PSQL "SELECT team_id FROM name='$WINNER'");
+    if [[ -z $WINNER_ID ]]
+    then 
+      $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+    fi
+
+  #----------------------------  
+    
+    OPPONENT_ID=$(PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+    if [[ -z $OPPONENT_ID ]]
+    then 
+      $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')") 
+    fi
+  fi
+done
+
+# ./insert_data.sh test 
